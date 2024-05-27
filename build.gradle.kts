1dad604f39b22e16f9f1094f3bd919691d4f5d80
Team Omega <100494326+EinfachEli08@users.noreply.github.com>
2024-05-28 00:04:41 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.compose")
+}
+
+group = "org.omega"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+
+    implementation(compose.desktop.currentOs)
+    implementation("org.jetbrains.compose.material3:material3-desktop:1.2.1")
+}
+
+
+
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "texture_packer_compose"
+            packageVersion = "1.0.0"
+        }
+    }
+}
